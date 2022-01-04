07f454d554df29936ceebae53848783455ae61dd
Tyler Cook <Tyler.INDY@GMail.com>
2022-01-04 13:38:42 -0500

    added React Router, working on ProductScreen

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,20 +1,25 @@
 import React from "react"
 import { Card, CardGroup } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import Rating from "./Rating"
 
 export default function Product({ product }) {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
-      </a>
+      </Link>
       <Card.Body>
         <Card.Title as='div'>
-          <strong>{product.name}</strong>
+          <Link to={`/product/${product._id}`}>
+            <strong>{product.name}</strong>
+          </Link>
         </Card.Title>
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
         <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>

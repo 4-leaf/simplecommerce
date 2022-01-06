59d1cdb2496eb57d21d686432437cfbcadfca46a
Tyler Cook <Tyler.INDY@GMail.com>
2022-01-06 15:55:00 -0500

    adding Router

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import colors from "colors"
 import connectDB from "./config/db.js"
 import products from "./data/products.js"
+import productRoutes from "./routes/productRoutes.js"
 
 dotenv.config()
 
@@ -12,15 +13,6 @@ const app = express()
 
 app.get("/", (req, res) => {
   res.send("API running....")
-})
-
-app.get("/api/products", (req, res) => {
-  res.json(products)
-})
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
 })
 
 const PORT = process.env.PORT || 5000
